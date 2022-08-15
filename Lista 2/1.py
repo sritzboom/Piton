3b4036206913de15aea0f39a502a1901ca71c6f6
Anderson Paulino <80299399+sritzboom@users.noreply.github.com>
2022-08-15 00:01:05 -0300

    Update 1.py

--- Lista 2/1.py
+++ Lista 2/1.py
@@ -6,21 +6,21 @@ somaFaren = 0
 listF = []
 
 
-for i in range(0,5):
+for i in range(0,50):
     temp = float(input('Temperatura em graus Celsius: '))
     somaCelsius += temp
     listF.append(temp*1.8+32)
     somaFaren += listF[i]
 
-mediaCelsius = somaCelsius/5
-mediaFaren = somaFaren/5
+mediaCelsius = somaCelsius/50
+mediaFaren = somaFaren/50
 
 print('Média das temperaturas em Célsius: ',mediaCelsius)
 print('Média das temperaturas em Farenheit: ',mediaFaren)
 
 ListaAcimaF = []
 
-for j in range(0,5):
+for j in range(0,50):
     if listF[j] > mediaFaren:
         ListaAcimaF.append(listF[j])
 print('Temperaturas acima da média em Farenhite', ListaAcimaF)
